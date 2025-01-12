7fd5d6c9dd868319fa9b8177400c3ff5dbdfed7c
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-12 22:24:29 +0100

    fix: replace -V (uppercase) with -v (lowercase) + use constant for version

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import { Command } from "commander";
 import init from "./core/init";
+import { VERSION } from "./utils/consts";
 const program = new Command();
 
 program
   .name("sshman")
   .description("A simple terminmal based SSH manager created in Node.js")
-  .version("0.1.0");
+  .version(VERSION, "-v, --version", "output the version number");
 
 program.parse();
 
