59de815f08bf1bbb5cf85a5d23a5a82a2e4aa86b
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-13 11:19:53 +0100

    fix(init script): update files + make function async

--- src/core/init.ts
+++ src/core/init.ts
@@ -2,19 +2,23 @@ import { existsSync } from "fs";
 import yoctoSpinner from "yocto-spinner";
 import createFileIfNotExists from "../utils/createFileIfNotExist";
 import { homedir } from "os";
+import { DEFAULT_CONFIG } from "../utils/consts";
 
-function init() {
-  const serversFile = `${homedir()}/.sshman/servers.json`;
-  let spinner = null;
-  if (!existsSync(serversFile)) {
-    console.log(
-      "This seems like your first time here. We will initialize things for you."
-    );
-    spinner = yoctoSpinner({ text: "Creating config files…" }).start();
-    createFileIfNotExists(serversFile);
+async function init() {
+  const configFile = `${homedir()}/.sshman/config.json`;
+  const logsFile = `${homedir()}/.sshman/logs.json`;
+  let spinner = yoctoSpinner({ text: "Checking config files…" }).start();
+  // check for app config files
+  if (!existsSync(configFile) || !existsSync(logsFile)) {
+    spinner.text = "Creating config files…";
+    await createFileIfNotExists(configFile, JSON.stringify(DEFAULT_CONFIG));
+    await createFileIfNotExists(logsFile, "[]");
+    spinner.text = "Config files created!";
   }
 
-  spinner?.success("Done!");
+  // todo: add config files validations
+
+  spinner?.success("App started!");
 }
 
 export default init;
