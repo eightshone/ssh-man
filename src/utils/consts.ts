0c7b818252f720d62dcf45f9d8f8ff01162148bc
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-13 11:18:02 +0100

    feat(utils): add default config object

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,1 +1,8 @@
+import { config } from "./types";
+
 export const VERSION: string = "0.1.0";
+export const DEFAULT_CONFIG: config = {
+  defaultPrivateKey: null,
+  servers: [],
+  recentServers: [],
+};
