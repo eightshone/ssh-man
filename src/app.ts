111538cabea8053187ff495bb5c22270366caa94
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-13 15:02:10 +0100

    feat(app): add command example

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { Command } from "commander";
 import init from "./core/init";
 import { VERSION } from "./utils/consts";
+import interactive from "./core/interactive";
+import goodbye from "./utils/goodbye";
 const program = new Command();
 
 program
@@ -8,11 +10,30 @@ program
   .description("A simple terminmal based SSH manager created in Node.js")
   .version(VERSION, "-v, --version", "output the version number");
 
-program.parse();
+program.action(app);
 
-app();
+program
+  .command("goodbye")
+  .description("says goodbye")
+  .action(() => {
+    goodbye();
+    process.exit();
+  });
+
+program.parse();
 
 async function app() {
   // intialize the cli app
   await init();
+
+  await interactive();
 }
+
+process.on("uncaughtException", (error) => {
+  if (error instanceof Error && error.name === "ExitPromptError") {
+    goodbye();
+  } else {
+    // Rethrow unknown errors
+    throw error;
+  }
+});
