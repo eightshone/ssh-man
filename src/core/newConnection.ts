a4dcdbe824dd7a89d81bfe09de6b9e9deb3275d8
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-18 13:32:27 +0100

    feat(newConnection): enable saving and updating config and logs for new connections

--- src/core/newConnection.ts
+++ src/core/newConnection.ts
@@ -1,9 +1,17 @@
 import select from "@inquirer/select";
-import promptSSHConfig from "./promptSSHConfig";
-import { menu } from "../utils/types";
-import sshConnection from "./ssh";
 
-async function newConnection(): Promise<menu> {
+import promptSSHConfig from "./promptSSHConfig";
+import { config, log, menu } from "../utils/types";
+import sshConnection from "./ssh";
+import saveFile from "../utils/saveFile";
+import { configDir } from "../utils/consts";
+import updateRecentServers from "../utils/updateRecentServer";
+import { formattedTime } from "../utils/time";
+
+async function newConnection(
+  config: config,
+  logs: log[]
+): Promise<[menu, config, log[]]> {
   const saveConnection: boolean | null = await select({
     message: "Save connection",
     choices: [
@@ -27,11 +35,22 @@ async function newConnection(): Promise<menu> {
 
   if (saveConnection !== null) {
     const sshConfig = await promptSSHConfig(saveConnection);
+    if (saveConnection) {
+      // save new server config to the list of configs
+      config.servers = [...config.servers, sshConfig];
+      config.recentServers = updateRecentServers(
+        config.recentServers,
+        sshConfig
+      );
+      logs = [{ time: formattedTime, server: sshConfig.name }, ...logs];
+      saveFile(`${configDir}/config.json`, config);
+      saveFile(`${configDir}/logs.json`, logs);
+    }
     console.clear();
     await sshConnection(sshConfig);
   }
 
-  return "main";
+  return ["main", config, logs];
 }
 
 export default newConnection;
