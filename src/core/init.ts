ae83c68440fd6c91a245ae7d7aebf60465537d82
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-18 13:28:49 +0100

    refactor(init): improve file handling and return structured config and logs

--- src/core/init.ts
+++ src/core/init.ts
@@ -1,12 +1,13 @@
 import { existsSync } from "fs";
 import yoctoSpinner from "yocto-spinner";
 import createFileIfNotExists from "../utils/createFileIfNotExist";
-import { homedir } from "os";
-import { DEFAULT_CONFIG } from "../utils/consts";
+import { configDir, DEFAULT_CONFIG } from "../utils/consts";
+import loadFile from "../utils/loadFile";
+import { config, json, log } from "../utils/types";
 
-async function init() {
-  const configFile = `${homedir()}/.sshman/config.json`;
-  const logsFile = `${homedir()}/.sshman/logs.json`;
+async function init(): Promise<{ config: config; logs: log[] }> {
+  const configFile = `${configDir}/config.json`;
+  const logsFile = `${configDir}/logs.json`;
   let spinner = yoctoSpinner({ text: "Checking config files…" }).start();
   // check for app config files
   if (!existsSync(configFile) || !existsSync(logsFile)) {
@@ -18,7 +19,18 @@ async function init() {
 
   // todo: add config files validations
 
+  // load config and logs
+  spinner.text = "Loading config files…";
+  const configObj = await loadFile(`${configDir}/config.json`);
+  const logsObj = await loadFile(`${configDir}/logs.json`);
+  spinner.text = "Config files loaded!";
+
   spinner?.success("App started!");
+
+  return {
+    config: configObj as config,
+    logs: logsObj as log[],
+  };
 }
 
 export default init;
