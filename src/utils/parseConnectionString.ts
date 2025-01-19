4bd831d01c050f26e83dc7400c913bcf465a52a6
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-19 13:44:53 +0100

    feat(utils): add function that parses a connection string and creates a shh2 connection config

---
+++ src/utils/parseConnectionString.ts
@@ -0,0 +1,32 @@
+import { homedir } from "os";
+import { server } from "./types";
+
+function parseConnectionString(connectionString: string): server {
+  const regex = /^(.*?)(?::(.*?))?@(.*?)(?::(\d+))?$/;
+  const match = connectionString.match(regex);
+
+  const sshConfig: Partial<server> = {
+    name: `${match[1]}@${match[3]}`,
+    username: match[1],
+    usePassword: match[2] ? true : false,
+    host: match[3],
+    port: match[4] ? parseInt(match[4], 10) : 22,
+  };
+
+  if (sshConfig.usePassword) {
+    sshConfig.password = match[2];
+  } else if (sshConfig.usePassword === false) {
+    // had to do this because of typescript
+    sshConfig.privateKey = `${homedir()}/.ssh/id_rsa`;
+  }
+
+  if (match) {
+    return sshConfig as server;
+  } else {
+    throw new Error(
+      "Invalid connection string format. Expected format: username[:password]@server[:port]"
+    );
+  }
+}
+
+export default parseConnectionString;
