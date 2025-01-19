29a991f566bdf858b21ff775a5ca8fc841a8034f
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-19 22:24:57 +0100

    feat(utils): add function that finds a server ina servers array

---
+++ src/utils/findServerIndex.ts
@@ -0,0 +1,12 @@
+import { server } from "./types";
+
+function findServerIndex(servers: server[], server: server) {
+  // this will check servers regardless of their names, just their configs
+  return servers.findIndex(
+    (currentServer) =>
+      JSON.stringify({ ...currentServer, name: "" }) ===
+      JSON.stringify({ ...server, name: "" })
+  );
+}
+
+export default findServerIndex;
