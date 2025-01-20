63868febaca6615d6cc0e991c771f7b506bf2f28
Eightsh One <eightsh.one@gmail.com>
2025-01-20 16:08:29 +0100

    fix(utils): export and import normalizeServerName function

--- src/utils/findServer.ts
+++ src/utils/findServer.ts
@@ -1,6 +1,7 @@
 import { server } from "./types";
+import normalizeServerName from "./normalizeServerName";
 
-function findServer(servers: server[] = [], name: string) {
+function findServer(servers: server[] = [], name: string): server | undefined {
   return servers.find(
     (srvr) => normalizeServerName(srvr.name) === normalizeServerName(name)
   );
--- src/utils/normalizeServerName.ts
+++ src/utils/normalizeServerName.ts
@@ -1,3 +1,5 @@
 function normalizeServerName(name: string): string {
   return name.toLowerCase().replaceAll(" ", "-");
 }
+
+export default normalizeServerName;
