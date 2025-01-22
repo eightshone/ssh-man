9c22c51add74c17c40c4e31aaefb54bee69b53a6
Eightsh One <eightsh.one@gmail.com>
2025-01-22 16:09:54 +0100

    feat(utils): add string padding function

---
+++ src/utils/stringPadding.ts
@@ -0,0 +1,19 @@
+function stringPadding(
+  input: string,
+  totalLength = 30,
+  paddingPosition: "start" | "end" = "end",
+  paddingCharacter: string = " "
+): string {
+  const trimmedStr =
+    input.length > totalLength ? input.slice(0, totalLength - 1) + "…" : input;
+  let paddedStr;
+  if (paddingPosition === "end") {
+    paddedStr = trimmedStr.padEnd(totalLength, paddingCharacter);
+  } else {
+    paddedStr = trimmedStr.padStart(totalLength, paddingCharacter);
+  }
+
+  return paddedStr;
+}
+
+export default stringPadding;
