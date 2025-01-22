a3f1d0bdeed3ceba1c773e915d192a8e02b45141
Eightsh One <eightsh.one@gmail.com>
2025-01-22 17:24:56 +0100

    feat(interactive): add ssh listing menu

--- src/core/listConnections.ts
+++ src/core/listConnections.ts
@@ -1,7 +1,71 @@
-import { menu } from "../utils/types";
+import select, { Separator } from "@inquirer/select";
+import colors from "yoctocolors-cjs";
+import { menu, server } from "../utils/types";
+import { inquirerTheme } from "../utils/themes";
+import stringPadding from "../utils/stringPadding";
 
-async function listConnections(): Promise<[menu]> {
-  return ["main"];
+async function listConnections(servers: server[]): Promise<[menu, string[]]> {
+  let options: string[] | null = null;
+
+  const recents = servers.length
+    ? [
+        {
+          name: colors.dim(`  #  ${stringPadding("Name")}  Config`),
+          disabled: " ",
+          value: null,
+        },
+        ...servers.map((server, index) => ({
+          name: `${stringPadding(
+            `${index + 1}`,
+            3,
+            "start",
+            "0"
+          )}  ${stringPadding(server.name)}  ${server.username}:[redacted]@${
+            server.host
+          }:${server.port}`,
+          value: `ssh-display ${JSON.stringify(server)}`,
+          description: "Connect to server",
+        })),
+      ]
+    : [
+        {
+          name: "  📭 No saved servers! Add a new server or connect to one and it will appear here",
+          value: "recent-sessions",
+          disabled: " ",
+        },
+      ];
+
+  console.log("🗃️ Saved servers");
+  console.log('   Use "Search" option to enter search mode');
+  let answer = await select({
+    message: "Saved servers",
+    choices: [
+      {
+        name: "🔍 Search",
+        value: "ssh-search",
+        description: "Enter search mode",
+      },
+      {
+        name: "↩️ Main menu",
+        value: "main",
+        description: "Return to main menu",
+      },
+      new Separator(" "),
+      ...recents,
+    ],
+    theme: inquirerTheme,
+    pageSize: 14,
+  });
+
+  if (answer !== "exit") {
+    console.clear();
+  }
+
+  if (answer.startsWith("ssh-display")) {
+    [answer, ...options] = answer.split(" ");
+  }
+
+  return [answer as menu, options];
 }
 
 export default listConnections;
