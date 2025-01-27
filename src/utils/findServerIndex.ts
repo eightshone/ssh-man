4e5e22226bc6d9d22f7a254aab3a750d7c70e57d
Eightsh One <eightsh.one@gmail.com>
2025-01-27 17:29:20 +0100

    feat(utils): omit id when comparing server configs

--- src/utils/findServerIndex.ts
+++ src/utils/findServerIndex.ts
@@ -5,8 +5,10 @@ function findServerIndex(servers: server[], server: server) {
   // this will check servers regardless of their names, just their configs
   return servers.findIndex(
     (currentServer) =>
-      JSON.stringify(orderJSONFieldsByName({ ...currentServer, name: "" })) ===
-      JSON.stringify(orderJSONFieldsByName({ ...server, name: "" }))
+      JSON.stringify(
+        orderJSONFieldsByName({ ...currentServer, name: "", id: null })
+      ) ===
+      JSON.stringify(orderJSONFieldsByName({ ...server, name: "", id: null }))
   );
 }
 
