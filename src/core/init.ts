448e64dec6de0357af8644e6ab924510fd13acd1
Eightsh One <eightsh.one@gmail.com>
2025-01-27 17:20:52 +0100

    feat(init): add server ids and clear logs for old versions

--- src/core/init.ts
+++ src/core/init.ts
@@ -1,9 +1,12 @@
 import { existsSync } from "fs";
 import yoctoSpinner from "yocto-spinner";
+import { nanoid } from "nanoid";
 import createFileIfNotExists from "../utils/createFileIfNotExist";
-import { CONFIG_DIR, DEFAULT_CONFIG } from "../utils/consts";
+import { CONFIG_DIR, DEFAULT_CONFIG, VERSION } from "../utils/consts";
 import loadFile from "../utils/loadFile";
 import { config, log } from "../utils/types";
+import compareVersions from "../utils/compareVersions";
+import saveFile from "../utils/saveFile";
 
 async function init(): Promise<{ config: config; logs: log[] }> {
   const configFile = `${CONFIG_DIR}/config.json`;
@@ -21,15 +24,35 @@ async function init(): Promise<{ config: config; logs: log[] }> {
 
   // load config and logs
   spinner.text = "Loading config files…";
-  const configObj = await loadFile(`${CONFIG_DIR}/config.json`);
-  const logsObj = await loadFile(`${CONFIG_DIR}/logs.json`);
+  const configObj: config = await loadFile(`${CONFIG_DIR}/config.json`);
+  let logsObj: log[] = await loadFile(`${CONFIG_DIR}/logs.json`);
   spinner.text = "Config files loaded!";
+
+  spinner.text = "Checking config compatibility…";
+  if (
+    !configObj.version ||
+    (!!configObj.version && compareVersions(VERSION, configObj.version) === -1)
+  ) {
+    spinner.text = "Migrating configs…";
+    if (!configObj.version) {
+      // this will only work for certain pre logs integrations version
+      logsObj = [];
+      await saveFile(`${CONFIG_DIR}/logs.json`, logsObj);
+
+      // assign server ids
+      configObj.servers.forEach((srv) => {
+        srv.id = nanoid();
+      });
+    }
+    configObj.version = VERSION;
+    await saveFile(`${CONFIG_DIR}/config.json`, configObj);
+  }
 
   spinner?.success("App started!");
 
   return {
-    config: configObj as config,
-    logs: logsObj as log[],
+    config: configObj,
+    logs: logsObj,
   };
 }
 
