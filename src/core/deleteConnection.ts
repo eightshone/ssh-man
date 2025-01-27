59f1d7e80037a7c9c2792e458e0e8d9ec21c4552
Eightsh One <36240779+eightshone@users.noreply.github.com>
2025-01-27 21:56:21 +0100

    feat(ui): add delete server prompt

---
+++ src/core/deleteConnection.ts
@@ -0,0 +1,40 @@
+import select from "@inquirer/select";
+import { CONFIG_DIR } from "../utils/consts";
+import saveFile from "../utils/saveFile";
+import { config, menu, server } from "../utils/types";
+
+async function deleteConnection(
+  initialConfig: config,
+  selectedServer: server,
+  serverIndex: number
+): Promise<[menu, string[]?]> {
+  const deleteServer: boolean = await select({
+    message: `Delete ${selectedServer.name}`,
+    choices: [
+      {
+        name: "🗑️ Confirm and delete",
+        value: true,
+      },
+      {
+        name: "🚫 Cancel",
+        value: false,
+      },
+    ],
+  });
+  if (deleteServer) {
+    const config: config = { ...initialConfig };
+    const { servers } = initialConfig;
+
+    servers.splice(serverIndex, 1);
+
+    config.servers = servers;
+
+    await saveFile(`${CONFIG_DIR}/config.json`, config);
+
+    return ["ssh-list"];
+  }
+
+  return ["ssh-display", [JSON.stringify(selectedServer), `${serverIndex}`]];
+}
+
+export default deleteConnection;
