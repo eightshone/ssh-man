00caedbb58e397da23e587655b93c90ac0122546
Eightsh One <eightsh.one@gmail.com>
2025-01-28 12:03:33 +0100

    chore(utils): bump version to 0.1.1 and format constants

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,7 +1,7 @@
 import { homedir } from "os";
 import { config } from "./types";
 
-export const VERSION: string = "0.1.0";
+export const VERSION: string = "0.1.1";
 export const DEFAULT_CONFIG: config = {
   defaultPrivateKey: null,
   servers: [],
