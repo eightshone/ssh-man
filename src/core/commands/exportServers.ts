a592fa6377b5762b8aee196e24228b5a1f82b893
Eightsh One <eightsh.one@gmail.com>
2025-04-18 16:33:42 +0100

    feat(cli): export servers config

--- src/core/commands/exportServers.ts
+++ src/core/commands/exportServers.ts
@@ -4,12 +4,11 @@ import { server } from "../../utils/types";
 import init from "../functions/init";
 import { existsSync } from "fs";
 import saveFile from "../../utils/saveFile";
-import createFileIfNotExists from "../../utils/createFileIfNotExist";
 
 async function exportServers(servers: string[] = [], options) {
   const fileName = options.name?.length
     ? options.name
-    : `config-export-${dayjs().format("YYYY-MM-DD-HH:mm:ss")}.cfg`;
+    : `config-export-${dayjs().format("YYYY-MM-DD---HH-mm-ss")}.cfg`;
   const { config } = await init({ silent: true });
   const normalizedServerNames = servers.map((serverName: string) =>
     normalizeServerName(serverName)
@@ -23,13 +22,10 @@ async function exportServers(servers: string[] = [], options) {
     );
   }
 
-  console.log(serverConfigs);
-  console.log(fileName);
-
   if (!options.force && existsSync(fileName)) {
     throw new Error("File exists!");
   } else {
-    await await createFileIfNotExists(fileName, JSON.stringify(serverConfigs));
+    await saveFile(fileName, serverConfigs);
   }
 
   process.exit();
