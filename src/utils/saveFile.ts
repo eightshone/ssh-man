68d055c2fa74372f14dea187d6eed9d1895ce7cb
Eightsh One <eightsh.one@gmail.com>
2025-04-18 15:01:53 +0100

    feat(utils): add support for custom error messages for saveFile function

--- src/utils/saveFile.ts
+++ src/utils/saveFile.ts
@@ -1,10 +1,14 @@
 import fs from "fs";
 
-async function saveFile(filePath: string, data: any): Promise<void> {
+async function saveFile(
+  filePath: string,
+  data: any,
+  errorMessage: string = "Error updating config file file:"
+): Promise<void> {
   try {
     await fs.promises.writeFile(filePath, JSON.stringify(data), "utf8");
   } catch (error) {
-    console.error("Error updating config file file:", error);
+    console.error(errorMessage, error);
   }
 }
 
