7350f8317825658ac576e161a56925d8b449c7f5
eightshone <eightsh.one@gmail.com>
2025-11-28 23:25:02 +0100

    fix(utils?readConfigFile): simplify error message

--- src/utils/readConfigFile.ts
+++ src/utils/readConfigFile.ts
@@ -1,12 +1,13 @@
 import { promises as fs } from "fs";
+import { exit } from "process";
 
 async function readConfigFile<T = unknown>(filePath: string): Promise<T> {
   try {
     const data = await fs.readFile(filePath, "utf-8");
     return JSON.parse(data) as T;
   } catch (err) {
-    console.error(`Error reading or parsing JSON file: ${filePath}`, err);
-    throw err;
+    console.error(`Error reading or parsing config file: ${filePath}`);
+    exit();
   }
 }
 
